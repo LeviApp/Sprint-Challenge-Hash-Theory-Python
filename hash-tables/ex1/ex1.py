11400419997b78569374613cf3a7cb5ac8f80220
Levi Appenfelder <levi.appenfelder@gmail.com>
2019-08-03 17:55:43 -0600

    ex1.py complete and passing tests

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -9,11 +9,28 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
+    answer = None
     """
     YOUR CODE HERE
     """
 
-    return None
+    for i in range(length):
+        try:
+            
+            if hash_table_retrieve(ht, weights[i]) == None:
+                hash_table_insert(ht, weights[i], [i, weights[i], limit - weights[i]])
+            
+            if weights[i] + hash_table_retrieve(ht, weights[i])[1] == limit and hash_table_retrieve(ht, weights[i])[0] != i :
+                answer = (i, hash_table_retrieve(ht, weights[i])[0])
+                
+            if hash_table_retrieve(ht, limit - weights[i]) != None and limit - weights[i] != weights[i]:
+               answer = (hash_table_retrieve(ht, weights[i])[0], hash_table_retrieve(ht, limit - weights[i])[0])
+
+        except:
+            continue
+
+    return answer
+
 
 
 def print_answer(answer):
